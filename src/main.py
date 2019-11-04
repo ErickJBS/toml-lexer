1144e159543d23b2623576c381319960dc02b4a6
ErickJBS <a311008@uach.mx>
2019-11-04 09:47:24 -0700

    ignoring comments

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from tokenizer import Tokenizer
-from utils import formatOutput
+from utils import formatOutput, ignoreComments
 from const import table, finals
 import fileinput
 
@@ -7,6 +7,8 @@ tokenizer = Tokenizer(table, finals)
 stdin = ''
 for line in fileinput.input():
     stdin = stdin + line
+
+stdin = ignoreComments(stdin)
 
 tokens = tokenizer.tokenize(stdin);
 result = list(tokens)
--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,16 @@ def formatOutput(result):
     return str(result).replace('[', '') \
     .replace(']', '').replace('\'', '') \
     .replace(' ', '').replace(',', '\n')
+
+def ignoreComments(input):
+    delete_mode = True
+    result = []
+    for c in input:
+        if c == '#':
+            delete_mode = True
+        if c == '\n':
+            delete_mode = False
+        if c != '#' and not delete_mode:
+            result.append(c)
+
+    return ''.join(result)
