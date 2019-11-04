b7ea90eec96931e045a97fa7685640d2ef6ba84b
ErickJBS <a311008@uach.mx>
2019-11-04 00:09:14 -0700

    automata upgrade

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,19 +1,17 @@
 class Tokenizer(object):
     """docstring for Tokenizer."""
 
-    def __init__(self, alpha, table, finals):
+    def __init__(self, table, finals):
         super(Tokenizer, self).__init__()
-        self.alpha = alpha
         self.table = table
         self.finals = finals
 
     def tokenize(self, input):
         current_state = 0
         for c in input:
-            if not c in self.alpha:
-                raise Exception(f'Character {c} does not belong to the alphabet')
             # Go to next state
             current_state = self.table[current_state][c]
             # Check if the current state is final and print its token
             if current_state in self.finals:
                 yield self.finals[current_state]
+                current_state = 0
